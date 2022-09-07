45a9ac48370c5c7839c04517cddc70c560830126
Grant Knief <gknief@mac.com>
2022-09-07 14:09:25 -0400

    added ejs file to render data from db

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
+const {MongoClient} = require("mongodb")
 const express = require("express")
+let db
 
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the homepage")
+app.set("view engine", "ejs")
+app.set("views", "./views")
+
+app.get("/", async (req, res) => {
+  const allAnimals = await db.collection("animals").find().toArray()
+  res.render("home", {allAnimals})
 })
 
 app.get("/admin", (req, res) => {
   res.send("This is the admin page")
 })
 
-app.listen(3000)
+async function start() {
+  const client = new MongoClient("mongodb://root:root@localhost:27017/AmazingMernApp?&authSource=admin")
+  await client.connect()
+  db = client.db()
+  app.listen(3000)
+}
+
+start()
+
+
